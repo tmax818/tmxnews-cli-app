63f756c195bda4af0017a0153ce394619f51390a
Tyler Maxwell <tmax818@mac.com>
2018-02-10 13:38:58 -0800

    modify format of cli output

--- tmxnews/lib/tmxnews/cli.rb
+++ tmxnews/lib/tmxnews/cli.rb
@@ -10,7 +10,7 @@ class Tmxnews::CLI
         #this is where you get the data...
         @article = Tmxnews::Article.today 
         @article.each.with_index(1) do |article, i|
-            puts "#{i}. #{article.title} - #{article.url}"
+            puts "#{i}. #{article.title}"
         end 
     end 
 
@@ -24,13 +24,19 @@ class Tmxnews::CLI
 
             if input.to_i > 0
                 the_article = @article[input.to_i-1]
-                puts "#{the_article.text} - #{the_article.url}"
+                puts "Headline: #{the_article.title}\n\n Summary: #{the_article.text}\n\n URL: #{the_article.url}"
             elsif input == "list"
                 list_articles
+            elsif input == "exit"
+                good_bye 
             else 
                 puts "Not sure what you want, type 'list', 'exit', or select an article..."
             end 
         end 
+    end 
+
+    def good_bye
+        puts "Thank you for using Tmxnews! Check in tomorrow for the headlines"
     end 
 
 end 
