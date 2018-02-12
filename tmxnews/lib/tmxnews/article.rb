14bf9363f13aca05b004cbde5522c7c48e1b04d2
Tyler Maxwell <tmax818@mac.com>
2018-02-12 09:21:43 -0800

    refactor article.rb

--- tmxnews/lib/tmxnews/article.rb
+++ tmxnews/lib/tmxnews/article.rb
@@ -1,37 +1,32 @@
-class Tmxnews::Article # don't understand the reason for this namespacing.. 
-    attr_accessor :title, :text, :url 
+class Tmxnews::Article
+    attr_accessor :title, :text, :url
 
-    def self.today          
-        # return instances of article
-        # scrape nyt and wsj and return articles based on data 
+    def self.today
         self.scrape_articles
-    end 
-    
-    
+    end
+
+
     def self.scrape_articles
         articles = []
 
         articles << self.scrape_nyt
-        articles << self.scrape_wsj 
+        articles << self.scrape_wsj
 
-        articles   # by the end, return array of articles ..
-    end 
+        articles
+    end
 
-    def self.scrape_nyt 
+    def self.scrape_nyt
         doc = Nokogiri::HTML(open("https://www.nytimes.com"))
-        title = doc.search("h2.story-heading").first.text
-        text = doc.search("article.story").first.css("li").text
-        url = doc.search("h2.story-heading").first.css("a").attr("href").value
-        #binding.pry
 
         article = self.new
         article.title = doc.search("h2.story-heading").first.text
         article.text = doc.search("article.story").first.css("li").text
         article.url = doc.search("h2.story-heading").first.css("a").attr("href").value
-        article 
-    end 
 
-    def self.scrape_wsj 
+        article
+    end
+
+    def self.scrape_wsj
         doc = Nokogiri::HTML(open("https://www.wsj.com"))
         title = doc.search("h3.wsj-headline").first.text
         text = doc.search("p.wsj-summary").first.text
@@ -42,6 +37,6 @@ class Tmxnews::Article # don't understand the reason for this namespacing..
         article.title = doc.search("h3.wsj-headline").first.text
         article.text = doc.search("p.wsj-summary").first.text
         article.url = doc.search("h3.wsj-headline").first.css("a").attr("href").value
-        article 
-    end 
-end 
+        article
+    end
+end
